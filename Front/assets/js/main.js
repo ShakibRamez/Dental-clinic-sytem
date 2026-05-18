06e28d28943e21bb1e2eeb9238a0ba408c2e092a
ShakibRamez <shakibramiz1@icloud.com>
2026-05-18 11:25:16 +0430

    all files created and dashboard completed

---
+++ Front/assets/js/main.js
@@ -0,0 +1,7 @@
+// فعال کردن لینک فعال در سایدبار
+document.querySelectorAll('.nav-link').forEach(link => {
+  link.addEventListener('click', function() {
+    document.querySelectorAll('.nav-link').forEach(l => l.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
