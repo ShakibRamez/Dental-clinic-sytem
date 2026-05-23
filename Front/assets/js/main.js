a560f2ba7b17dc93f694aba6bbae1cfb0b5e92e9
ShakibRamez <shakibramiz1@icloud.com>
2026-05-24 00:06:07 +0430

    function added

--- Front/assets/js/main.js
+++ Front/assets/js/main.js
@@ -5,3 +5,9 @@ document.querySelectorAll('.nav-link').forEach(link => {
     this.classList.add('active');
   });
 });
+
+//اجازه ورد به صفحات فقط در صورت لاگین بودن
+  if (localStorage.getItem("isLoggedIn") !== "true") {
+      alert("شما وارد سیستم نشده‌اید! لطفاً ابتدا لاگین کنید.");
+      window.location.href = "login.html";
+  }
